ca2e0f3b46fa01dc027b651068be002be3756dbe
Farhana Aktar <farhanaaktar@live.co.uk>
2024-07-31 19:52:19 +0100

    exceeded rate limit so I could not see the styling or check for errors

--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,13 +1,23 @@
 import React from "react"
+import styled from 'styled-components'
+
+
+
+const StyledFigure = styled.figure`
+max-width: 300px
+
+figcaption {
+font-size:1.5em;}
+`
 
 
 export default function Figure({imageURL, caption}){
     return(
-      <figure>
+      <StyledFigure>
      <img src = {imageURL}/>
      <figcaption>Awesome pic taken on {caption}</figcaption>
     
-      </figure>
+      </StyledFigure>
     )
     
     }
